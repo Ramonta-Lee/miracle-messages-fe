5041a1295e14019ad72103a7d7963c75072d0405
Ramonta Lee <viola4lfe@gmail.com>
2020-10-02 17:08:23 -0400

    updated axios to correct baseURL

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosWithAuth = () => {
-    
-    let baseUrl;
-    if(process.env.NODE_ENV === 'development') {
-        baseUrl = `http://localhost:5000/`;
-    }  else {
-        baseUrl = `https://miracle-messages-dev.herokuapp.com/`;
-    }
+  let baseUrl;
+  if (process.env.NODE_ENV === "development") {
+    baseUrl = `http://localhost:5000/`;
+  } else {
+    baseUrl = `https://miracle-messages2.herokuapp.com/`;
+  }
 
-    return axios.create({
-        baseURL: baseUrl,
-        // headers: {
-        //     authorization: token
-        // }
-    });
+  return axios.create({
+    baseURL: baseUrl,
+    // headers: {
+    //     authorization: token
+    // }
+  });
 };
