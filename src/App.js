c8c145aa2890ecbafbb8f1c289be787f5804212c
Ramonta Lee <viola4lfe@gmail.com>
2020-10-04 15:27:22 -0400

    updated BaseURL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react"
-
+import React from "react";
 
 //Router
-import { Route, Switch, Redirect, useHistory } from "react-router-dom"
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 
 //Styles
-import "./CSS/App.css"
-import "./CSS/style.css"
+import "./CSS/App.css";
+import "./CSS/style.css";
 
 //Imported Components
 import Map from "./Components/MapComponents/map";
@@ -15,17 +14,17 @@ import VolunteerForm from "./Components/Forms/VolunteerForm";
 import LoginForm from "./Components/Login/LoginForm.js";
 import LoginPage from "./Components/Login/UserLogin";
 import NewChapterInfo from "./Components/Forms/NewChapterInfo";
-import ChapterForm from "./Components/Forms/ChapterForm"
-import ForgotPasswordForm from "./Components/Forms/ForgotPassword/ForgotPasswordForm"
-import ConfirmPassword from "./Components/Forms/ForgotPassword/ConfirmPassword"
-import SearchBar from "./Components/MapComponents/SearchBar.js"
-import NewVolunteer from './Components/Forms/onboarding/NewVolunteer';
-import ChapterPage from './Components/ChapterPage/ChapterPage';
+import ChapterForm from "./Components/Forms/ChapterForm";
+import ForgotPasswordForm from "./Components/Forms/ForgotPassword/ForgotPasswordForm";
+import ConfirmPassword from "./Components/Forms/ForgotPassword/ConfirmPassword";
+import SearchBar from "./Components/MapComponents/SearchBar.js";
+import NewVolunteer from "./Components/Forms/onboarding/NewVolunteer";
+import ChapterPage from "./Components/ChapterPage/ChapterPage";
 
 //Octa Import
 
-import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
-import config from './config/config.js';
+import { Security, SecureRoute, LoginCallback } from "@okta/okta-react";
+import config from "./config/config.js";
 //Custom Imports
 import { PrivateRoute } from "./Components/PrivateRoute";
 
@@ -37,62 +36,58 @@ import { PrivateRoute } from "./Components/PrivateRoute";
 // };
 
 //SHAWN OKTA
-import CustomLogin from './Components/Login/CustomLogin';
-
-
+import CustomLogin from "./Components/Login/CustomLogin";
 
 const App = () => {
   const history = useHistory();
   const onAuthRequired = () => {
     // Redirect to the /login page that has a CustomLoginComponent
-    history.push('/admin/login');
+    history.push("/admin/login");
   };
-  
-    return (
-      <div className="App">
-        <Security 
-        issuer='https://dev-662389.okta.com/oauth2/default'
-        clientId='0oa4sulf3qW8OTVdF4x6'
-        redirectUri={window.location.origin + '/implicit/callback'}
+
+  return (
+    <div className="App">
+      <Security
+        issuer="https://dev-662389.okta.com/oauth2/default"
+        clientId="0oa4sulf3qW8OTVdF4x6"
+        redirectUri={window.location.origin + "/implicit/callback"}
         onAuthRequired={onAuthRequired}
         pkce={true}
-        >
+      >
         {/* Routes */}
-          <Switch>
-            <Route exact path="/" render={props => <Map {...props} />} />
+        <Switch>
+          <Route exact path="/" render={(props) => <Map {...props} />} />
 
-            <Route exact path="/form" component={VolunteerForm} />
+          <Route exact path="/form" component={VolunteerForm} />
 
-            <Route exact path="/chapter-page" component={ChapterPage} />
-            
+          <Route exact path="/chapter-page" component={ChapterPage} />
 
-            <Route exact path="/user/login" component={LoginPage} />
-            <Route exact path="/user/Register" component={NewVolunteer} />
-            {/* <Route exact path="/admin/login" component={LoginForm} /> */}
-            <Route exact path='/admin/login' render={() => <LoginForm baseUrl='https://dev-636641.okta.com' />} />
-            <Route path='/implicit/callback' component={LoginCallback}/>
+          <Route exact path="/user/login" component={LoginPage} />
+          <Route exact path="/user/Register" component={NewVolunteer} />
+          {/* <Route exact path="/admin/login" component={LoginForm} /> */}
+          <Route
+            exact
+            path="/admin/login"
+            render={() => <LoginForm baseUrl="https://dev-636641.okta.com" />}
+          />
+          <Route path="/implicit/callback" component={LoginCallback} />
 
-            
-              
-              <Route exact path="/user/newchapter" component={NewChapterInfo} />
-              <Route exact path="/user/newchapterform" component={ChapterForm} />
-              
-            
-            
-            {/* Takes to searchbar component  */}
-            <Route path="/searchbar" component={SearchBar} />
+          <Route exact path="/user/newchapter" component={NewChapterInfo} />
+          <Route exact path="/user/newchapterform" component={ChapterForm} />
 
-            {/* <PrivateRoute path="/admin" component={Fulllayout} /> */}
-            {/* <SecureRoute path="/admin" component={Fulllayout} /> */}
-            {/* <SecureRoute path="/admin" render={props => <FullLayout {...props} />} /> */}
-            <SecureRoute path="/admin" component={FullLayout} />
+          {/* Takes to searchbar component  */}
+          <Route path="/searchbar" component={SearchBar} />
 
+          {/* <PrivateRoute path="/admin" component={Fulllayout} /> */}
+          {/* <SecureRoute path="/admin" component={Fulllayout} /> */}
+          {/* <SecureRoute path="/admin" render={props => <FullLayout {...props} />} /> */}
+          <SecureRoute path="/admin" component={FullLayout} />
 
-            <Redirect from="*" to="/" />
-          </Switch>
-        </Security>
-      </div>
-    );
-}
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Security>
+    </div>
+  );
+};
 
 export default App;
